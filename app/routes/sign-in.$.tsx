8150a51d97e13b5f9defb410ecf1d60eec2da36c
Terrance-TT <yungyungadam@gmail.com>
2026-07-22 04:23:54 -0400

    revert: remove sign-in route from main (moved to clerk-auth branch)

--- app/routes/sign-in.$.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from '@clerk/remix';
-
-export default function SignInPage() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-bolt-elements-background-depth-1">
-      <SignIn routing="path" path="/sign-in" signUpUrl="/sign-up" />
-    </div>
-  );
-}
